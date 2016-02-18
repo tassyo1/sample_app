df51ce6410203a2e0091fd166ce62378e4a07d88
Tássyo Santana <tassyo.santana@hotmail.com>
2016-02-18 17:49:16 -0200

    corrigindo routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  root 'application#hello'
-=======
   root 'static_pages#home'
   get 'static_pages/help'
   get 'static_pages/about'
 
 
->>>>>>> static-pages
 end
