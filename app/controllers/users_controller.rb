7685e946a82b99e6a702ea7a5947f0e3b4372fc9
Tássyo Santana <tassyo.santana@hotmail.com>
2016-03-05 20:16:43 -0300

    Pausa no início do tópico ' 7.3.4 A test for invalid submission'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+   # debugger (comando da gem byebug)
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Mostra mensagem de sucesso
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
