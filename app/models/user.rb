7b48eca19e600f4df166088e49a6d0fdb20c5925
Tássyo Santana <tassyo.santana@hotmail.com>
2016-03-09 14:19:21 -0300

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
   validates_uniqueness_of :email , :case_sensitive => false
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }  
+  validates :password, presence: true, length: { minimum: 6 } 
+ 
 end
