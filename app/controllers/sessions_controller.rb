814774e8c822c3bdf0433d6ad2f3f59f5b5dae07
Tássyo Santana <tassyo.santana@hotmail.com>
2016-04-22 21:12:59 -0300

    Terminado log in/ log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-      log_in user
+     log_in user     
+     #guarda o remember token e o user id ambos criptografados em cookies:w
+     params[:session][:remember_me] == '1' ?  remember(user) : forget(user)
      redirect_to user  
     else
       #Create a new error message.
@@ -15,7 +17,7 @@ class SessionsController < ApplicationController
    end
 
   def destroy
-    log_out
+    log_out if logged_in? #caso o usuário tenha duas telas abertas e deslogar em uma
     redirect_to root_url
   end
 end
